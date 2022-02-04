e3274813ea8968b420adb127f15237f9a4df051a
PoojaSivakumar97 <58037293+poojaSivakumar97@users.noreply.github.com>
2022-02-05 00:16:02 +0530

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -3,30 +3,35 @@ import {
   Text,
   SafeAreaView,
   ScrollView,
-  StyleSheet,
-  Image,
-  TouchableOpacity,
   TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
 } from "react-native";
-import React from "react";
 import Feather from "react-native-vector-icons/Feather";
+import React, { useState } from "react";
 import Carousel from "react-native-snap-carousel";
-import { sliderData } from "../Models/data";
+import { sliderData, freeGames, paidGames } from "../Models/data";
 import { windowWidth } from "../utlis/Dimensions";
 import BannerSlider from "../components/BannerSlider";
+import CustomSwitch from "../components/CustomSwitch";
+import ListItem from "../components/ListItem";
 
 const Home = () => {
+  const [gamesTab, setGamestab] = useState(1);
   const renderBanner = ({ item, index }) => {
     return <BannerSlider data={item} />;
   };
+  const onSelectSwitch = (value) => {
+    setGamestab(value);
+  };
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "#fff", paddingTop: 10 }}>
-      <ScrollView style={{ padding: 20 }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+      <ScrollView style={{ padding: 20, marginTop: 20 }}>
         <View style={styles.headerContainer}>
-          <Text style={{ fontSize: 25, fontWeight: "bold", color: "#20315f" }}>
+          <Text style={{ fontSize: 22, color: "#20315f", fontWeight: "bold" }}>
             Hello Joey!
           </Text>
-
           <TouchableOpacity>
             <Image
               source={require("../assets/images/user-profile.jpg")}
@@ -34,21 +39,16 @@ const Home = () => {
             />
           </TouchableOpacity>
         </View>
-        <View style={styles.searchContainer}>
-          <Feather
-            name="search"
-            color={"#C6C6C6"}
-            size={22}
-            style={{ marginRight: 6 }}
-          />
+        <View style={styles.searchConatiner}>
+          <Feather name="search" size={22} color="#C6C6C6" />
           <TextInput placeholder="search" />
         </View>
         <View style={styles.headerContainer}>
-          <Text style={{ fontSize: 18, fontWeight: "bold", color: "#20315f" }}>
+          <Text style={{ fontSize: 18, color: "#20315f", fontWeight: "bold" }}>
             Upcoming Games
           </Text>
           <TouchableOpacity>
-            <Text style={{ color: "#0aada8", fontSize: 18 }}>See all</Text>
+            <Text style={{ color: "#0aada8" }}>see all</Text>
           </TouchableOpacity>
         </View>
         <Carousel
@@ -61,26 +61,40 @@ const Home = () => {
           itemWidth={300}
           loop={true}
         />
+        <CustomSwitch
+          selectionMode={1}
+          option1={"Free Games"}
+          option2={"Paid Games"}
+          onSelectSwitch={onSelectSwitch}
+        />
+        <View style={{ marginBottom: 30 }}>
+          {gamesTab == 1 &&
+            freeGames.map((game) => {
+              return <ListItem key={game.id} data={game} />;
+            })}
+          {gamesTab == 2 &&
+            paidGames.map((game) => {
+              return <ListItem key={game.id} data={game} />;
+            })}
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
 };
-
 const styles = StyleSheet.create({
   headerContainer: {
-    flexDirection: "row",
-    // paddingTop: 20,
     marginVertical: 20,
+    flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
   },
-  searchContainer: {
+  searchConatiner: {
     flexDirection: "row",
-    borderRadius: 3,
-    borderColor: "#C6C6C6",
     padding: 5,
+    borderRadius: 5,
+    borderColor: "#C6C6C6",
     borderWidth: 1,
-    // marginBottom: 20,
   },
 });
+
 export default Home;
