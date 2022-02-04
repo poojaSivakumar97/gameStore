c8113f73f7657e6d0c55b43910c77ed48d61535f
PoojaSivakumar97 <58037293+poojaSivakumar97@users.noreply.github.com>
2022-02-05 00:15:46 +0530

    Create ListItem.js

---
+++ components/ListItem.js
@@ -0,0 +1,53 @@
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+
+import React from "react";
+import { windowWidth } from "../utlis/Dimensions";
+
+const ListItem = ({ data }) => {
+  return (
+    <View style={styles.gameDetails}>
+      <View style={{ flexDirection: "row" }}>
+        <Image
+          source={data.poster}
+          style={{ height: 55, width: 50, borderRadius: 5 }}
+        />
+        <View style={{ marginLeft: 10, width: windowWidth - 220 }}>
+          <Text style={{ fontSize: 14 }}>{data.subtitle}</Text>
+          <Text
+            numberOfLines={1}
+            style={{ fontSize: 16, textTransform: "uppercase" }}
+          >
+            {data.title}
+          </Text>
+        </View>
+      </View>
+      <TouchableOpacity
+        style={{
+          backgroundColor: "#0aada8",
+          borderRadius: 10,
+          width: 80,
+          height: 30,
+          justifyContent: "center",
+          alignItems: "center",
+          //   marginVertical: 20,
+          //   marginBottom: 20,
+        }}
+      >
+        <Text style={{ fontSize: 16, color: "#fff" }}>
+          {data.isFree == "Yes" ? "Free" : data.price}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  gameDetails: {
+    flex: 1,
+    flexDirection: "row",
+    paddingHorizontal: 20,
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+});
+
+export default ListItem;
