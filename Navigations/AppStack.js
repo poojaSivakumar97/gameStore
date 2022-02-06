b8cbcb9d6587e47d4bcb9e7c763cf75e9db88c84
PoojaSivakumar97 <58037293+poojaSivakumar97@users.noreply.github.com>
2022-02-06 15:33:26 +0530

    Update AppStack.js

--- Navigations/AppStack.js
+++ Navigations/AppStack.js
@@ -1,15 +1,74 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import Ionicons from "react-native-vector-icons/Ionicons";
 import Home from "../screens/Home";
+import ProfileScreen from "../screens/ProfileScreen";
+import MessagesScreen from "../screens/MessagesScreen";
+import MomentsScreen from "../screens/MomentsScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+import CustomDrawer from "../components/CustomDrawer";
+import { color } from "react-native-reanimated";
 
 const Drawer = createDrawerNavigator();
 
 const AppStack = () => {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Home" component={Home} />
-      {/* <Drawer.Screen name="Article" component={Article} /> */}
+    <Drawer.Navigator
+      drawerContent={(props) => <CustomDrawer {...props} />}
+      screenOptions={{
+        headerShown: false,
+        drawerLabelStyle: { marginLeft: -25 },
+        drawerActiveTintColor: "#fff",
+        drawerActiveBackgroundColor: "#aa18ea",
+        drawerInactiveTintColor: "#333",
+      }}
+    >
+      <Drawer.Screen
+        name="Home"
+        component={Home}
+        options={{
+          drawerIcon: ({ color }) => (
+            <Ionicons name="home-outline" size={18} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          drawerIcon: ({ color }) => (
+            <Ionicons name="person-outline" size={18} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Message"
+        component={MessagesScreen}
+        options={{
+          drawerIcon: ({ color }) => (
+            <Ionicons name="chatbox-ellipses-outline" size={18} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Moments"
+        component={MomentsScreen}
+        options={{
+          drawerIcon: ({ color }) => (
+            <Ionicons name="timer-outline" size={18} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          drawerIcon: ({ color }) => (
+            <Ionicons name="settings-outline" size={18} color={color} />
+          ),
+        }}
+      />
     </Drawer.Navigator>
   );
 };
