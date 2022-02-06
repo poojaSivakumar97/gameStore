7d4f4d9da87bc0e82a45d81391aaf92245a068a7
PoojaSivakumar97 <58037293+poojaSivakumar97@users.noreply.github.com>
2022-02-06 15:33:36 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import "react-native-gesture-handler";
 import { View, Text } from "react-native";
 import React from "react";
+import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import AuthStack from "./Navigations/AuthStack";
 import AppStack from "./Navigations/AppStack";
